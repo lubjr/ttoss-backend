584ccbb807a7e3bfc0ca480f0f94bbcb348a7352
Luiz Eduardo <luizeduardobjr@outlook.com>
2024-03-05 16:13:44 -0300

    01_connectionwithdynamo

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import express from 'express'
+import AWS from 'aws-sdk'
+import { awsConfig } from '../config/credentials'
+
+const app = express()
+
+const table = 'Videos'
+AWS.config.update(awsConfig)
+const dynamodb = new AWS.DynamoDB.DocumentClient()
+
+app.get('/', async () => {
+  return { hello: 'world' }
+})
+
+app.post('/videos', async (req, res) => {
+  const { pk, sk, url } = req.query
+  const params = {
+    TableName: table,
+    Item: {
+      pk,
+      sk,
+      url,
+    },
+  }
+  try {
+    await dynamodb.put(params).promise()
+    res.status(200).send('Video added')
+  } catch (err) {
+    res.status(400).send('Error adding video')
+  }
+})
+
+app.listen(3333, () => {
+  console.log('Server is running on port 3333')
+})
