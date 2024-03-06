ddc4d4bf94f49c14858651c75e664970e7a77ac4
Luiz Eduardo <luizeduardobjr@outlook.com>
2024-03-06 07:32:50 -0300

    #03_removecredentials

--- config/credentials.ts
+++ config/credentials.ts
@@ -8,6 +8,6 @@ export interface AwsConfig {
 export const awsConfig: AwsConfig = {
   region: 'us-east-1',
   endpoint: 'https://dynamodb.us-east-1.amazonaws.com',
-  accessKeyId: 'AKIAWGFPMNYCB7M6FEWF',
-  secretAccessKey: '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',
+  accessKeyId: '',
+  secretAccessKey: '',
 }
