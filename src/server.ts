cd5f01bb8b8a95c229db682178503a8c2c805778
Luiz Eduardo <luizeduardobjr@outlook.com>
2024-03-07 08:30:48 -0300

    #11_testprisma

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import express from 'express'
 import router from '../routes/routersdynamo'
 
+import { PrismaClient } from '@prisma/client'
+
 const app = express()
+const prisma = new PrismaClient()
 
 app.use(express.json())
 app.use(router)
 
-app.get('/', async () => {
-  return { hello: 'world' }
+app.get('/', async (req, res) => {
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
+
+  res.send({ hello: 'world' })
 })
 
 app.listen(3333, () => {
